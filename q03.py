1b40942324748a8192b3df840d88e1564ed77858
kuma <kumamonmaster@gmail.com>
2021-06-12 08:55:23 +0900

    add another pattern to the q3

--- q03.py
+++ q03.py
@@ -1,3 +1,18 @@
+# パターン1
+N = 100
+cards = [False for i in range(N)]
+
+for i in range(2, N):
+    j = i -1
+    while j < len(cards):
+        cards[j] = not cards[j]
+        j += i
+
+for i in range(N):
+    if not cards[i]:
+        print(i+1)
+
+# パターン2
 for i in range(1, 101):
     flag = False
     for j in range(1, 101):
