e94b1e0c9c8bef4ec4645615b91c48b3e7535f5f
kuma <kumamonmaster@gmail.com>
2021-06-12 09:12:09 +0900

    bug fix to patter1 on the q3

--- q03.py
+++ q03.py
@@ -2,21 +2,21 @@
 N = 100
 cards = [False for i in range(N)]
 
-for i in range(2, N):
+for i in range(2, N+1):
     j = i -1
     while j < len(cards):
         cards[j] = not cards[j]
         j += i
 
-for i in range(N):
-    if not cards[i]:
+for i, card in enumerate(cards):
+    if not card:
         print(i+1)
 
 # パターン2
-for i in range(1, 101):
-    flag = False
-    for j in range(1, 101):
-        if i % j == 0:
-            flag = not flag
-    if flag:
-        print(i)
+# for i in range(1, 101):
+#     flag = False
+#     for j in range(1, 101):
+#         if i % j == 0:
+#             flag = not flag
+#     if flag:
+#         print(i)
